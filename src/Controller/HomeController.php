1ce322345e623186d34ab71c72b8607ceff36346
unknown <gdv.delvecchio75@gmail.com>
2022-03-04 03:39:04 +0100

    add create and edit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Offers;
+use App\Form\OfferType;
+use App\Form\NewofferType;
 use App\Repository\OffersRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,11 +14,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
+     * @var OffersRepository
+     */
+    private $repository;
+
+    public function __construct(OffersRepository $repository, EntityManagerInterface $em){
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
      * @Route("/", name="home")
      */
-    public function index(OffersRepository $repository): Response
+    public function index(): Response
     {
-        $offers = $repository->findAll();
+        $offers = $this->repository->findAll();
         return $this->render('home/index.html.twig', [
             'offers' => $offers,
         ]);
@@ -22,9 +37,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/offres/{id}", name="offer.show")
      */
-    public function show($id, OffersRepository $repository): Response
+    public function show($id): Response
     {
-        $offer = $repository->find($id);
+        $offer = $this->repository->find($id);
         return $this->render('home/show.html.twig', [
             'offer' => $offer,
         ]);
@@ -32,8 +47,41 @@ class HomeController extends AbstractController
     /**
      * @Route("/créer", name="offer.create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        return $this->render('home/create.html.twig');
+        $offer = new Offers();
+
+        $form = $this->createForm(NewofferType::class, $offer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($offer);
+            $this->em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('home/create.html.twig', [
+            'offer' => $offer,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/éditer/{id}", name="offer.edit")
+     */
+    public function edit($id, Request $request): Response
+    {
+        $offer = $this->repository->find($id);
+
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/edit.html.twig', [
+            'offer' => $offer,
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/NewofferType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Offers;
+use App\Entity\BenefitType;
+use App\Entity\TypeOfDelivery;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+
+class NewofferType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Creator', TextType::class)
+            ->add('amount', MoneyType::class, array(
+                'scale' => 2,
+                'currency' => false,
+            ))
+            ->add('delivery')
+            ->add('offers_delivery', EntityType::class, array(
+                'class' => TypeOfDelivery::class,
+                'choice_label' => 'name',
+                'expanded' => false,
+                'multiple' => false
+            ))
+            ->add('offer_benefit', EntityType::class, array(
+                'class' => BenefitType::class,
+                'choice_label' => 'name',
+                'expanded' => false,
+                'multiple' => false
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Offers::class,
+        ]);
+    }
+}
